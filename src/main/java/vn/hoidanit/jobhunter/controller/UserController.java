03f8e58c963e3603d4b6376770602c8708347b01
hoangkhp <116267046+hoangkhp@users.noreply.github.com>
2024-10-17 23:17:43 +0700

    delete and read User

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -1,12 +1,16 @@
 package vn.hoidanit.jobhunter.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.UserService;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
@@ -18,12 +22,29 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/user/createUser")
+    @PostMapping("/user")
     public User createNewUser(
         @RequestBody User postmanUser 
         ) {
         User createUser = this.userService.saveUser(postmanUser);
         return createUser;
     }
+
+    @DeleteMapping("/user/{id}")
+    public String createNewUser(
+        @PathVariable("id") Long id
+    ){
+        this.userService.handleDeleteUser(id);
+        return "delete User";
+    }
+
+    @GetMapping("/user/{id}")
+    public User readAllUser(
+        @PathVariable("id") Long id
+    ){
+        User readUser = this.userService.handleGetAllUser(id);
+        return readUser;
+    }
+    
     
 }
--- src/main/java/vn/hoidanit/jobhunter/service/UserService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/UserService.java
@@ -16,4 +16,12 @@ public class UserService {
     public User saveUser(User user){
          return this.userRepository.save(user);
     }
+
+    public void handleDeleteUser(Long id){
+         this.userRepository.deleteById(id);
+    }
+
+    public User handleGetAllUser(Long id){
+        return this.userRepository.findById(id).get();
+    }
 }
